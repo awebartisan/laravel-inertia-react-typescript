49be431db72a4350d79cc8aba37917a1acd77e8f
Zubair Mohsin <awebartisan@gmail.com>
2020-09-12 17:30:43 +0500

    removes react-preset and modifications to initialization of Inertia

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ const mix = require("laravel-mix");
  |
  */
 
-mix.ts("resources/js/app.js", "public/js")
+mix.ts("resources/js/app.tsx", "public/js")
     .postCss("resources/css/app.css", "public/css", [
         //
     ])
